ac09c9afddab6d85c184dbf419316a67d231cde1
jake-bukovich <64559413+jake-bukovich@users.noreply.github.com>
2020-12-10 12:35:39 -0700

    Update server.js

--- JacobBukovich_FinalExam/server.js
+++ JacobBukovich_FinalExam/server.js
@@ -1,32 +1,42 @@
 
-
-
-var express = require('express'); 
+var express = require('express'); //Ensure our express framework has been added
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());             
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-
-var pgp = require('pg-promise')();
-var app = express();
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
 
-let dbConfig = {
-  host: 'localhost',
-  port: 5432,
-  database: 'search',
-  user: 'postgres',
-  password: 'pwd'
-};
-
-const isProduction = process.env.NODE_ENV === 'production';
-dbConfig = isProduction ? process.env.DATABASE_URL : dbConfig;
-let db = pgp(dbConfig);
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));
 
+
+app.get('/', function (req, res) {
+
+  var sql = require("mssql");
+  
+  var config = {
+    user: 'jake-bukovich',
+    password: 'abc',
+    server: 'localhost',
+    database: 'uni-reviews'
+  };
+
+  sql.connect(config, function (err) {
+
+    if (err) console.log(err);
+
+    var request = new sql.Request();
+
+    request.query('select * from reviews', function (err, recordset) {
+
+      if (err) console.log(err)
+
+      res.send(recordset);
+
+    });
+  });
+});
 
 app.get('/', function (req, res) {
   res.render('pages/main', {
@@ -52,12 +62,12 @@ app.get('/main', function(req,res){
 
 });
 
+
+
 //app.listen(3000);
 
-const PORT = process.env.PORT || 8080;
-
-const server = app.listen(PORT, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
+var server = app.listen(5000, function () {
+  console.log('Server is running...');
 });
 
 
@@ -68,29 +78,3 @@ const server = app.listen(PORT, () => {
 
 
 
-/*$(function () {
-  $("form[role='search']").submit(function (event) {
-    event.preventDefault();
-    var s = $("input[type='text']", this).val;
-
-    var url = "http://universities.hipolabs.com/search?name=" + s;
-    $.ajax({ url: url, dataType: "jsonp" }).then(function (data) {
-      console.log(data);
-      var search = data.completedSearch[0].result[0].search;
-      var ser = "<div class='row'>";
-      for (var i = 0; i < search.length; i++) {
-        ser += "<div class='column>";
-        ser += "<div class='card'>";
-        ser += "<h2>" + search[i].name + "</h2>";
-        ser += "<p>" + search[i].country + "</p>";
-        ser += "<a href= '" + search[i].webpage + "' class = 'button web-button'>University Website</a>";
-        ser += "</div></div>";
-
-
-      };
-      $('.results').html(ser);
-    });
-
-  });
-});
-*/
