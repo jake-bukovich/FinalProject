c5a944678fe099ad5b03d4199f1e8f7a6c344bd0
jake-bukovich <jabu0530@colorado.edu>
2020-12-10 02:30:16 -0700

    delete

--- JacobBukovich_FinalExam/server.js
+++ JacobBukovich_FinalExam/server.js
@@ -5,8 +5,7 @@
   Body-Parser  - A tool to help use parse the data in a post request
   Pg-Promise   - A database tool to help use connect to our PostgreSQL database
 ***********************/
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+
 var express = require('express'); //Ensure our express framework has been added
 var app = express();
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
